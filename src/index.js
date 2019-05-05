a794bba9cdb379e75c67548a75bfa1001e82b830
Andrey Ivanov <andrey-tm203@mail.ru>
2019-05-05 17:00:49 +0300

    render movies list, add prop-types check

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app.jsx';
 
+const moviesList = [`Fantastic Beasts`, `Happy Gilmore`, `Major Payne`, `Mission: Impossible`];
+
 const init = () => {
   const root = document.querySelector(`#root`);
-  ReactDOM.render(<App />, root);
+
+  ReactDOM.render(<App moviesList={moviesList} />, root);
 };
 
 init();
